02f984fb270b944fa1204c2107143045f99fc082
Matthew Iasevoli <mi580745@wne.edu>
2022-07-15 16:39:38 -0400

    rudimentary movement and dashing for the player

--- GMTK 2022/Assets/PlayerMovement.cs
+++ GMTK 2022/Assets/PlayerMovement.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private PlayerControls system;
+    private Rigidbody rb;
+    private Vector2 moveVec;
+    private int dashCoolDown = 0;
+
+    [SerializeField]
+    private float moveSpeed;
 
     private void OnEnable()
     {
@@ -19,7 +25,8 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         system = new PlayerControls();
-        system.InGame.Move.performed += ctx => MovePlayer(ctx.ReadValue<Vector2>());
+        system.InGame.Move.performed += ctx => moveVec = ctx.ReadValue<Vector2>();
+        system.InGame.Move.canceled += ctx => moveVec = Vector2.zero;
         system.InGame.PrimaryFire.performed += ctx => PrimaryFire();
         system.InGame.SecondaryFire.performed += ctx => SecondaryFire();
         system.InGame.Dash.performed += ctx => Dash();
@@ -28,18 +35,22 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        dashCoolDown -= dashCoolDown > 0 ? 1 : 0;
+        MovePlayer(moveVec * moveSpeed);
     }
 
     void MovePlayer(Vector2 moveControls)
     {
-
+        Vector2 locationVec;
+        locationVec = transform.position;
+        locationVec += moveControls;
+        transform.position = locationVec;
     }
 
     void PrimaryFire()
@@ -54,6 +65,10 @@ public class PlayerMovement : MonoBehaviour
 
     void Dash()
     {
-
+        if (dashCoolDown == 0)
+        {
+            MovePlayer(moveVec * 5);
+            dashCoolDown = 1200;
+        }
     }
 }
