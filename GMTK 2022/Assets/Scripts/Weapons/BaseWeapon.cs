ccbb169fb1862139fefce8d1a5c3aa78d96770b1
Brennan Zynda <brennan.zynda@mymail.champlain.edu>
2022-07-15 13:52:16 -0400

    Weapon implementation base

---
+++ GMTK 2022/Assets/Scripts/Weapons/BaseWeapon.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseWeapon : MonoBehaviour
+{
+    public float damageValue;
+    public float attackSpeed;
+    public float attackRange;
+    public float knockbackAmount;
+
+    public abstract void UseAbility();
+}
